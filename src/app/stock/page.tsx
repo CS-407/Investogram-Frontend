78777bca5fa585d6058005122b8a9e4c2bd56b81
AJ Glodowski <ajglodo@gmail.com>
2023-02-06 21:55:42 -0500

    Sell button and updating buy button UI

--- src/app/stock/page.tsx
+++ src/app/stock/page.tsx
@@ -1,10 +1,12 @@
-import BuyButton from "../../components/buybutton";
+import SellButton from "@/components/stock/sellbutton";
+import BuyButton from "@/components/buybutton";
 
 export default function stock() {
     return (
       <main className="">
         <div className="underline">stock page</div>
         <BuyButton />
+        <SellButton />
       </main>
     )
   }
--- src/components/stock/sellbutton.tsx
+++ src/components/stock/sellbutton.tsx
@@ -2,10 +2,11 @@
 
 import { useState } from 'react';
 
-export default function BuyButton() {
+export default function SellButton() {
 
     const mockUser = {
-        currentBalance: 250
+        currentBalance: 1000,
+        currentStockOwned: 10
     }
 
     const mockStock = {
@@ -17,11 +18,11 @@ export default function BuyButton() {
     const [orderAmt, setOrderAmt] = useState(0);
 
     function availableBalance() {
-        return mockUser.currentBalance - (orderAmt * mockStock.price)
+        return mockUser.currentBalance + (orderAmt * mockStock.price)
     }
 
     function decreaseButton() {
-        if (orderAmt > 0) {
+        if (orderAmt > 0) { // Don't go below 0
             return (
                 <button 
                 className="flex items-center justify-center px-2 py-1 text-base font-medium leading-6 text-white whitespace-no-wrap bg-black border-2 border-transparent rounded-full shadow-sm hover:bg-transparent hover:text-black hover:border-black focus:outline-none"
@@ -31,9 +32,12 @@ export default function BuyButton() {
             )
         }
     }
+
+    // Black button with white text that flips color on hover
+    
     
     function increaseButton() {
-        if (availableBalance() - mockStock.price >= 0) { // Enough funds
+        if (orderAmt+1 <= mockUser.currentStockOwned) { // Enough funds
             return (
                 <button 
                 className="flex items-center justify-center px-2 py-1 text-base font-medium leading-6 text-white whitespace-no-wrap bg-black border-2 border-transparent rounded-full shadow-sm hover:bg-transparent hover:text-black hover:border-black focus:outline-none"
@@ -54,42 +58,46 @@ export default function BuyButton() {
         )
     }
 
+    function remainingStock() {
+        let remaining = mockUser.currentStockOwned - orderAmt
+        return (
+            <div className="text-sm italic p-1">{remaining} shares left worth ${(remaining * mockStock.price).toFixed(2)}</div>
+             
+        )
+    }
+
+
+
     function selectSection() {
-        if (availableBalance() < 0) {
-            return <div className="text-sm p-1">Insufficient funds</div>
-        } else {
-            if (orderAmt > 0) {
-                return ( 
-                    <div className='flex-col py-2 px-2'>
-                        <button 
-                        className="flex-row items-center justify-center px-4 py-2 text-2xl font-semibold leading-6 text-white whitespace-no-wrap bg-green-500 border-2 border-transparent rounded-full shadow-sm hover:bg-transparent hover:text-green-500 hover:border-green-500 focus:outline-none"
-                        onClick={() => console.log("buy")}
-                        >
-                            <div className='flex justify-center'>
-                                Buy
-                                <div className='pl-2 italic'> {mockStock.ticker} </div>
-                            </div>
-                            <div className="flex-row font-normal italic text-xs p-1">{orderAmt} shares for ${(orderAmt * mockStock.price).toFixed(2)}</div>
-                        </button>
-                        
-                    </div>
-                )
-            }
+        if (orderAmt > 0) {
+            return ( 
+                <div className='flex-col items-center px-2'>
+                    <button 
+                    className="flex-row items-center justify-center px-4 py-2 text-2xl font-semibold leading-6 text-white whitespace-no-wrap bg-red-500 border-2 border-transparent rounded-full shadow-sm hover:bg-transparent hover:text-red-500 hover:border-red-500 focus:outline-none"
+                    onClick={() => console.log("sell")}
+                    >
+                        Sell
+                        <div className="flex-row font-normal text-xs italic p-1">Sell {orderAmt} shares for ${(orderAmt * mockStock.price).toFixed(2)}</div>
+                    </button>
+                </div>
+            )
+
         }
     }
 
 
     return (
       <main>
-        <div className="inline-block border-solid rounded-md border-2 border-green-500 align-middle text-center">
+        <div className="inline-block border-solid rounded-md border-2 border-red-500 align-middle text-center">
             <div className="flex font-bold text-2xl p-1">
-                Buy {mockStock.name} (
+                Sell {mockStock.name} (
                     <div className='italic'>{mockStock.ticker}</div>
                 )
             </div>
             {amtButtons()}
+            {remainingStock()}
             {selectSection()}
-            <div className="text-sm p-1">${availableBalance().toFixed(2)} left to spend</div>
+            <div className="text-sm p-1">${availableBalance().toFixed(2)} in account balance</div>
         </div>
       </main>
     )
