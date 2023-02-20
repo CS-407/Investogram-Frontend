bda2daaeb866f77d1dedfee34469a6a1e03101c6
AJ Glodowski <ajglodo@gmail.com>
2023-02-20 18:50:18 -0500

    Connected buy button with backend using hardcoded objects

--- src/components/buybutton.tsx
+++ src/components/buybutton.tsx
@@ -4,6 +4,36 @@ import { useState } from 'react';
 
 export default function BuyButton() {
 
+    const currentUser = '63dd552ebafd3a998d66b5ee';
+    const currentPrice = '63dd56e4f7c1c8cf06522dc9';
+    const currentStock = '63dd56b9f7c1c8cf06522dc8';
+
+    const executeBuy = async () => {
+        fetch('http://localhost:8080/api/stock/buy', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                user_id: currentUser,
+                stock_id: currentStock,
+                stock_price_id: currentPrice,
+                no_of_shares: orderAmt,
+                amount_usd: orderAmt * mockStock.price,
+                buy: true
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if (data.msg === "Success") {
+                setBuySuccess(true);
+            } else {
+                setBuyFail(true);
+            }
+        })
+    };
+
     const mockUser = {
         currentBalance: 250
     }
@@ -63,7 +93,12 @@ export default function BuyButton() {
                     <div className='flex-col py-2 px-2'>
                         <button 
                         className="flex-row items-center justify-center px-4 py-2 text-2xl font-semibold leading-6 text-white whitespace-no-wrap bg-green-500 border-2 border-transparent rounded-full shadow-sm hover:bg-transparent hover:text-green-500 hover:border-green-500 focus:outline-none"
-                        onClick={() => console.log("buy")}
+                        onClick={
+                            () => {
+                                console.log("Buying stock")
+                                executeBuy();
+                            }
+                        }
                         >
                             <div className='flex justify-center'>
                                 Buy
@@ -78,20 +113,31 @@ export default function BuyButton() {
         }
     }
 
+    const [buySuccess, setBuySuccess] = useState(false);
+    const [buyFail, setBuyFail] = useState(false);
 
-    return (
-      <main>
-        <div className="inline-block border-solid rounded-md border-2 border-green-500 align-middle text-center">
-            <div className="flex font-bold text-2xl p-1">
-                Buy {mockStock.name} (
-                    <div className='italic'>{mockStock.ticker}</div>
-                )
+    if (buySuccess) {
+        return (
+            <div className="inline-block border-solid rounded-md border-2 border-green-500 align-middle text-center">
+                <div className='text-lg font-bold p-2'>Buy Success</div>
             </div>
-            {amtButtons()}
-            {selectSection()}
-            <div className="text-sm p-1">${availableBalance().toFixed(2)} left to spend</div>
-        </div>
-      </main>
-    )
+        )
+    } else {
+        return (
+        <main>
+            <div className="inline-block border-solid rounded-md border-2 border-green-500 align-middle text-center">
+                <div className="flex font-bold text-2xl p-1">
+                    Buy {mockStock.name} (
+                        <div className='italic'>{mockStock.ticker}</div>
+                    )
+                </div>
+                {amtButtons()}
+                {selectSection()}
+                <div className="text-sm p-1">${availableBalance().toFixed(2)} left to spend</div>
+                {buyFail && <div className="text-sm p-1 text-red-500">ERROR: Stock Buy Failure</div>}
+            </div>
+        </main>
+        )
+    }
   }
   
