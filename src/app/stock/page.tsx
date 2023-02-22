10cfd10b32b7bb746abae9dff62694c09d862487
Mihir Abh <abhyankm@purdue.edu>
2023-02-22 18:47:24 -0500

    ignore2

--- src/app/stock/page.tsx
+++ src/app/stock/page.tsx
@@ -1,10 +1,9 @@
-import BuyButton from "../../components/buybutton";
+
 
 export default function stock() {
     return (
       <main className="">
         <div className="underline">stock page</div>
-        <BuyButton />
       </main>
     )
   }
