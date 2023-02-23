b26f6cdd91e42a7554a2d167857b3415ec793fda
PrekshaRavi811 <ptharigo@purdue.edu>
2023-02-22 23:06:48 -0500

    commit1

--- src/app/popular-stocks/page.tsx
+++ src/app/popular-stocks/page.tsx
@@ -1,7 +1,13 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+
 export default function popular_stocks() {
     return (
-      <main className="">
-        <div>Popular Stocks</div>
+      <main className={styles.main}>
+        <div> 
+          <h1> Popular Stocks </h1>
+        </div>
       </main>
     )
   }
