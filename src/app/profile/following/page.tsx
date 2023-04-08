1bf3863cd4fcf032e345a70bee02c43bb87d40fc
vvegesna01 <kvegesna01@gmail.com>
2023-04-08 01:35:55 -0400

    update followers/following list

--- src/app/profile/following/page.tsx
+++ src/app/profile/following/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
+import { User } from "@/util/types";
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "src/util/globals";
+
+import axios from "axios";
 
 const Following = () => {
+
+  const [followingList, setFollowingList] = useState([]);
+  const [followingNumber, setFollowingNumber] = useState(0);
+
+  useEffect(() => {
+      axios.get(`${BASE_URL}/api/user/following`, {
+        headers: {
+          "Authorization": "Bearer " + localStorage.getItem("token")
+      }
+      })
+      .then(response => {
+        const data = response.data;
+        setFollowingList(data);
+        setFollowingNumber(data.length);
+      })
+      .catch(error => {
+        console.log(error);
+        alert('Trouble contacting server');
+      })
+  }, []) // Added an empty dependency array to ensure the useEffect hook is only executed once on component mount.
+
   return (
-    <div>List of users current user is following</div>
+    <div>
+      <h1 className="text-2xl font-bold mt-4">Users You Follow</h1>
+      <p>Number of following: {followingNumber}</p>
+      <ul>
+        {followingList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
+
 
 export default Following;
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,21 +5,30 @@ import Link from "next/link";
 
 import RecentTradesSection from "../../components/profile/recentTradesSection";
 import LossGainSection from "@/components/profile/lossGainSection";
+import Followers from "./followers/page";
+import Following from "./following/page";
 
 
 export default function profile() {
+
 	return (
+
 		<main className="">
 			<div className="flex flex-row">
-			<div className="flex-none w-1/3 p-4">
-			<img src={"default_profile.png"} alt={`${"Username"}'s avatar`} className="w-48 h-48 rounded-full" />
+			<div className="flex-center w-1/3 p-4">
+			<img src={"public/default_profile.png"} alt={`${"Username"}'s avatar`} className="w-48 h-48 rounded-full" />
 			<h1 className="text-2xl font-bold mt-4">{"Username"}</h1>
 			<div className="flex flex-row">
-				<div className="text-gray-500 mt-2 w-1/2 p-1">{10} followers</div>
-				<div className="text-gray-500 mt-2 w-1/2 p-1">{10} following</div>
+				<div className="text-gray-500 mt-2 w-1/2 p-1">{ 
+					<Link href={"/profile/followers"}> {} followers </Link>
+					} 
+				</div>
+				<div className="text-gray-500 mt-2 w-1/2 p-1">{ 
+					<Link href={"/profile/following"}> {} following</Link>
+				} </div>
 			</div>
-				<div className="text-gray-500 mt-2">{
-					<button className="flex items-center justify-center px-2 py-1 text-base font-medium leading-6 text-white whitespace-no-wrap bg-black border-2 border-transparent rounded-full shadow-sm hover:bg-transparent hover:text-black hover:border-black focus:outline-none">
+				<div className="text-gray-200 mt-2">{
+					<button className="flex items-center justify-center px-2 py-1 text-base font-small leading-6 text-white whitespace-no-wrap bg-black border-2 border-transparent rounded-full shadow-sm hover:bg-transparent hover:text-black hover:border-black focus:outline-none">
 						<Link href={"/profile/requests"}>See Follow Requests</Link>
 					</button>
 				}
@@ -29,9 +38,7 @@ export default function profile() {
 				<div>
 				{<RecentTradesSection />}
 				</div>
-				<div>
-					Portfolio
-				</div>
+				<h1 className="text-4l font-bold mt-4">{"My Portfolio"}</h1>
 			</div>
 
 			
