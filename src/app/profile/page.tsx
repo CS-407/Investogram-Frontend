1b8ef6dcdd8ab0cceb4d0eb42cdb4c9a3806afd8
vvegesna01 <kvegesna01@gmail.com>
2023-04-08 15:20:12 -0400

    update profile components

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -8,32 +8,59 @@ import LossGainSection from "@/components/profile/lossGainSection";
 import { useContext } from "react";
 import AuthContext from "@/context/AuthContext";
 
+import followersNumber from "./followers/page";
+import followingNumber from "./following/page";
+
 export default function profile() {
 	const { user } = useContext(AuthContext);
 
+  
 	return (
-
-		<main className="">
+	  <main className="" style={{ backgroundColor: "#f5f5f5", padding: "20px" }}>
+		<div className="flex flex-row">
+		  <div className="flex-none w-1/3 p-4 flex justify-center items-center flex-col" style={{backgroundColor: "#f5f5f5",  padding: "20px" }}>
+			<img
+			  src={"public/images/default_profile.jpg"}
+			  alt={`${user?.username}'s avatar`}
+			  className={"flex-center"}
+			  style={{ borderRadius: "50%", width: "150px", height: "150px", objectFit: "cover" }}
+			/>
+			<h1 className="text-2xl font-bold mt-4" style={{ color: "#364F6B" }}>
+			  {user?.username}
+			</h1>
 			<div className="flex flex-row">
-				<div className="flex-none w-1/3 p-4">
-					<img src={"public/images/default_profile.jpg"} alt={`${user?.username}'s avatar`} className="w-48 h-48 rounded-full" />
-					<h1 className="text-2xl font-bold mt-4">{user?.username}</h1>
-					<div className="flex flex-row">
-						<div className="text-gray-500 mt-2 w-1/2 p-1">{user?.followers} followers</div>
-						<div className="text-gray-500 mt-2 w-1/2 p-1">{user?.following} following</div>
-					</div>
-					<div className="text-gray-500 mt-2">
-						{
-							<button className="flex items-center justify-center px-2 py-1 text-base font-medium leading-6 text-white whitespace-no-wrap bg-black border-2 border-transparent rounded-full shadow-sm hover:bg-transparent hover:text-black hover:border-black focus:outline-none">
-								<Link href={"/profile/requests"}>See Follow Requests</Link>
-							</button>
-						}
-					</div>
-				</div>
-				<div className="flex-grow w-2/3 p-4">
-					<div><RecentTradesSection user_id={""} /></div>
-				</div>
+			  <div className={`followers`} style={{ marginRight: "10px", color: "#364F6B" }}>
+			  <button className={`button`} style={{ border: "none", backgroundColor: "transparent", cursor: "pointer", color: "#666" }}>
+				<Link href={"/profile/followers"} style={{ textDecoration: "none" }}>
+				{user?.followers} {followersNumber} followers
+				</Link>
+			  </button>
+				
+			  </div>
+			  <div className={`following`} style={{ color: "#364F6B" }}>
+			  <button className={`button`} style={{ border: "none", backgroundColor: "transparent", cursor: "pointer", color: "#666" }}>
+				<Link href={"/profile/following"} style={{ textDecoration: "none" }}>
+				{user?.following} {followingNumber} following
+				</Link>
+			  </button>
+				
+			  </div>
 			</div>
-		</main>
+			<div className="text-black-500 mt-2">
+			  <button className={"flex items-center justify-center px-2 py-1 text-base font-medium leading-6 text-white whitespace-no-wrap bg-black border-2 border-transparent rounded-full shadow-sm hover:bg-transparent hover:text-black hover:border-black focus:outline-none"}>
+				<Link href={"/profile/requests"} style={{ textDecoration: "none" }}>
+				  See Follow Requests
+				</Link>
+			  </button>
+			</div>
+		  </div>
+		  <div className="flex-grow w-2/3 p-4" style={{backgroundColor: "#FDE698"}}>
+			<div>
+			  <RecentTradesSection user_id={""} />
+			</div>
+		  </div>
+		</div>
+	  </main>
 	);
-}
+  }
+  
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -18,6 +18,7 @@ const Navbar = () => {
 					<li>
 						<Link href={"/"}>
 							<p className={`block ${pathname == "/" ? "font-bold" : ""} hover:underline py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500`}>
+								{/* <img src = {"src/components/navbar/investogram_logo.png"}></img> */}
 								Home
 							</p>
 						</Link>
