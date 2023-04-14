5996ec6596b1eb3b50f809a4a5bef8f19fd73c89
vvegesna01 <kvegesna01@gmail.com>
2023-04-14 14:05:26 -0400

    fixes to main

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -69,7 +69,7 @@ const Navbar = () => {
 				{authCtx.isAuth ? (
 					<ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 						<li className="my-auto">
-							<Link href={"/profile"}>
+							{/* <Link href={"/profile"}>
 								<p
 									className={`block ${
 										pathname == "/profile" ? "font-bold" : ""
@@ -77,7 +77,7 @@ const Navbar = () => {
 								>
 									Profile
 								</p>
-							</Link> 
+							</Link>  */}
 							<ProfileDropdown/>
 							
 						</li>
