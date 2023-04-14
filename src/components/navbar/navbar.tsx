73a98443f4dfa139d2a9506ead5cf75bbc6a470c
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2023-04-13 21:03:52 -0400

    added link to homepage on navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,7 +5,6 @@ import React, { useContext } from "react";
 import { usePathname, useRouter } from "next/navigation";
 
 import AuthContext from "@/context/AuthContext";
-import { Colors } from "chart.js";
 
 const Navbar = () => {
 	const authCtx = useContext(AuthContext);
@@ -13,41 +12,51 @@ const Navbar = () => {
 	const pathname = usePathname();
 
 	return (
-
 		<nav className="bg-gray dark:bg-gray-900 w-full sticky z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
 			<div className="flex flex-wrap items-center justify-between mx-auto p-4">
 				<ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 					<li>
+						<Link href={"/"}>
 							<p
 								className={`block ${
 									pathname == "/" ? "font-bold" : ""
 								} hover:underline py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500`}
-									style={{backgroundColor:"#FDE698"}}>
-								<img className="h-10"
-									src={"/images/logo-remove-bg.png"}
-								></img>
+								style={{ backgroundColor: "#FDE698" }}
+							>
+								<img className="h-10" src={"/images/logo-remove-bg.png"}></img>
 								{/* Investogram */}
-
 							</p>
-
+						</Link>
 					</li>
 					<li>
 						<Link href={"/globalstocks"}>
-							<p className={`block ${pathname == "/globalstocks" ? "font-bold" : ""} hover:underline py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500`}>
+							<p
+								className={`block ${
+									pathname == "/globalstocks" ? "font-bold" : ""
+								} hover:underline py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500`}
+							>
 								Global Stocks
 							</p>
 						</Link>
 					</li>
 					<li>
 						<Link href={"/globalusers"}>
-							<p className={`block ${pathname == "/globalusers" ? "font-bold" : ""} hover:underline py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500`}>
+							<p
+								className={`block ${
+									pathname == "/globalusers" ? "font-bold" : ""
+								} hover:underline py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500`}
+							>
 								Global Users
 							</p>
 						</Link>
 					</li>
 					<li>
 						<Link href={"/leaderboard"}>
-							<p className={`block ${pathname == "/leaderboard" ? "font-bold" : ""} hover:underline py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500`}>
+							<p
+								className={`block ${
+									pathname == "/leaderboard" ? "font-bold" : ""
+								} hover:underline py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500`}
+							>
 								Leaderboard
 							</p>
 						</Link>
@@ -57,7 +66,11 @@ const Navbar = () => {
 					<ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 						<li className="my-auto">
 							<Link href={"/profile"}>
-								<p className={`block ${pathname == "/profile" ? "font-bold" : ""} hover:underline py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500`}>
+								<p
+									className={`block ${
+										pathname == "/profile" ? "font-bold" : ""
+									} hover:underline py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500`}
+								>
 									Profile
 								</p>
 							</Link>
@@ -83,7 +96,6 @@ const Navbar = () => {
 				)}
 			</div>
 		</nav>
-
 	);
 };
 
