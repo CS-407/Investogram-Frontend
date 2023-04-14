24ccaa80f174f828369dbf62fd78f7a402643d75
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2023-04-13 20:53:51 -0400

    added pages for updating username/password

--- src/app/globalstocks/page.tsx
+++ src/app/globalstocks/page.tsx
@@ -1,23 +1,24 @@
 import SearchStock from "@/components/stock/searchStock";
 
 export default function globalstocks() {
-    return (
-        <main
-				className="p-5"
-				style={{ backgroundColor: "#f5f5f5"}}
+	return (
+		<main className="p-5" style={{ backgroundColor: "#f5f5f5" }}>
+			<div
+				className="flex-none flex justify-center items-center flex-col rounded-lg shadow-lg"
+				style={{ backgroundColor: "#FDE698", padding: "20px" }}
 			>
-                <div
-						className="flex-none flex justify-center items-center flex-col rounded-lg shadow-lg"
-						style={{ backgroundColor: "#FDE698", padding: "20px" }}
-					>
-        <h1
-							className="text-5xl font-bold mt-4 mb-4"
-							style={{ color: "#364F6B"}}
-				>Global Stocks</h1>
-                <p>Navigate to a stock page you see here or use the search bar to find a stock!</p>
-                </div>
-                <SearchStock />
-        </main>
-        
-    )
+				<h1
+					className="text-5xl font-bold mt-4 mb-4"
+					style={{ color: "#364F6B" }}
+				>
+					Global Stocks
+				</h1>
+				<p>
+					Navigate to a stock page you see here or use the search bar to find a
+					stock!
+				</p>
+			</div>
+			<SearchStock />
+		</main>
+	);
 }
