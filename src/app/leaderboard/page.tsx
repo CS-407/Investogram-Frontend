4428fe58ef5411d7b6886dc881e32a8f311b7274
vvegesna01 <kvegesna01@gmail.com>
2023-04-18 10:01:07 -0400

    notifs update

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -2,6 +2,21 @@
 
 export default function leaderboard() {
     return (
-		<p>This is the leaderboard page</p>
+      <main
+      className="p-5"
+      style={{ backgroundColor: "#f5f5f5"}}
+  >
+                      <div
+          className="flex-none flex justify-center items-center flex-col rounded-lg shadow-lg"
+          style={{ backgroundColor: "#FDE698", padding: "20px" }}
+        >
+<h1
+                  className="text-5xl font-bold mt-4 mb-4"
+                  style={{ color: "#364F6B"}}
+      >Leaderboard</h1>
+      <p></p>
+      </div>
+
+      </main>
     )
 }
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -23,9 +23,9 @@ const Navbar = () => {
 									pathname == "/" ? "font-bold" : ""
 								} hover:underline py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500`}
 									style={{backgroundColor:"#FDE698"}}>
-								<img className="h-10"
+								<Link href="/"><img className="h-10"
 									src={"/images/logo-remove-bg.png"}
-								></img>
+								></img></Link>
 								{/* Investogram */}
 
 							</p>
@@ -61,6 +61,7 @@ const Navbar = () => {
 									Profile
 								</p>
 							</Link>
+				
 						</li>
 						<li>
 							<button
