40400c24363b73a08d4c4027cb168f1962d6750a
AJ Glodowski <ajglodo@gmail.com>
2023-04-19 18:12:15 -0400

    Adding stock categories section to global stocks

--- src/app/globalstocks/page.tsx
+++ src/app/globalstocks/page.tsx
@@ -1,16 +1,11 @@
 import SearchStock from "@/components/stock/searchStock";
+import { StockCategories } from "@/components/stock/stockCategories";
 
 export default function globalstocks() {
 	return (
-		<main className="p-5" style={{ backgroundColor: "#f5f5f5" }}>
-			<div
-				className="flex-none flex justify-center items-center flex-col rounded-lg shadow-lg"
-				style={{ backgroundColor: "#FDE698", padding: "20px" }}
-			>
-				<h1
-					className="text-5xl font-bold mt-4 mb-4"
-					style={{ color: "#364F6B" }}
-				>
+		<main className="p-5 bg-white">
+			<div className="flex-none flex justify-center items-center flex-col rounded-lg shadow-lg bg-investogram_yellow py-4 text-investogram_navy">
+				<h1 className="text-5xl font-bold mt-4 mb-4">
 					Global Stocks
 				</h1>
 				<p>
@@ -18,6 +13,7 @@ export default function globalstocks() {
 					stock!
 				</p>
 			</div>
+			<StockCategories />
 			<SearchStock />
 		</main>
 	);
