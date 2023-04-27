fc036d8226d242255a3d843431f43d36c9b6da73
PrekshaRavi811 <ptharigo@purdue.edu>
2023-04-27 18:07:16 -0400

    aggregate stocks + popular stats

--- src/app/stock/[stockId]/page.tsx
+++ src/app/stock/[stockId]/page.tsx
@@ -4,6 +4,7 @@ import BuyButton from "@/components/stock/buybutton";
 import UserTradesSection from "@/components/stock/userTradesSection";
 import StockGraph from "@/components/stock/stockGraph";
 import FriendsList from "@/components/stock/friendsList";
+import PopularStats from "@/components/stock/popularStats";
 import { Purchase } from "@/util/types";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -126,6 +127,7 @@ export default function stock() {
 			</div>
 
 			<UserTradesSection stockId={stockId} />
+			<PopularStats stockId={stockId} />
 			<FriendsList stockId={stockId} />
 		</main>
 	);
