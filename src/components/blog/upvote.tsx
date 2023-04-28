67c86fda02c4abcfb2c8feab4a54ed0bbf650f1e
Mihir Abh <abhyankm@purdue.edu>
2023-04-28 11:15:56 -0400

    Upvote and view other blogs working

--- src/components/blog/upvote.tsx
+++ src/components/blog/upvote.tsx
@@ -47,7 +47,7 @@ const UpvoteButton = (props: React.PropsWithChildren<{ upvoteBlog: (like: number
         <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
             <button type="button" className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-green-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-500"
                 onClick={handleChange}>
-                    {isLike ? "Unlike" : "Like"}
+                    {isLike ? "Like" : "Unlike"}
             </button>
         </div>
     )
